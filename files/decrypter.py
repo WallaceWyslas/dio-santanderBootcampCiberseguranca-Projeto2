e56187ebf9cb25f73436112efc98769594849a89
Wallace Wyslas <wallacewyslas@gmail.com>
2024-11-07 14:46:20 -0300

    fixed erros

--- files/decrypter.py
+++ files/decrypter.py
@@ -16,7 +16,7 @@ descrypt_data = aes.decrypt(file_data)
 os.remove(file_name)
 
 # Create a new descrypted file
-new_file = 'teste.txt'
+new_file = 'test.txt'
 new_file = open(f'{new_file}', 'wb')
 new_file.write(descrypt_data)
 new_file.close()
